2e339503172fcc153a9a0ebbb936ce1dd90a89b7
RamonMarM <120236024+RamonMarM@users.noreply.github.com>
2024-08-28 20:58:00 -0300

    Update views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,8 @@
-from flask import render_template, session, redirect, url_for, current_app
-from .. import db
-from ..models import User
-from ..email import send_email, send_simple_message
+from flask import render_template, session, redirect, url_for
 from . import main
+from .. import db
+from ..templates.models import User  # Importação ajustada para a nova localização
 from .forms import NameForm
-
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
@@ -16,16 +14,10 @@ def index():
             db.session.add(user)
             db.session.commit()
             session['known'] = False
-            print('FLASKY_ADMIN: ' + str(current_app.config['FLASKY_ADMIN']), flush=True)
-            if current_app.config['FLASKY_ADMIN']:
-                #send_email(current_app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
-                print('Enviando mensagem...', flush=True)
-                send_simple_message([current_app.config['FLASKY_ADMIN'], "flaskaulasweb@zohomail.com"], 'Novo usuário', form.name.data)
-                print('Mensagem enviada...', flush=True)
         else:
             session['known'] = True
         session['name'] = form.name.data
         return redirect(url_for('.index'))
-    return render_template('index.html',
-                           form=form, name=session.get('name'),
-                           known=session.get('known', False))
+    usuarios = User.query.all()
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known=session.get('known', False), usuarios=usuarios)
