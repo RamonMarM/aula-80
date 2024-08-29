010f87a0fc37a2411ce574959559266ffcf97a72
RamonMarM <120236024+RamonMarM@users.noreply.github.com>
2024-08-28 21:17:17 -0300

    Update __init__.py

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,5 +1,11 @@
-from flask import Blueprint
+from flask import Flask
 
-main = Blueprint('main', __name__)
+def create_app(config_name):
+    app = Flask(__name__)
 
-from . import views, errors
+    # Adicione suas configurações e inicializações aqui
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
